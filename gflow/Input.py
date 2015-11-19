93733129a53efd646adeb2a17bb5d7116b7cea8d
Alex MacLean <maclean199@gmail.com>
2015-11-19 13:17:35 -0500

    Removed directories to make pull request

--- gflow/Input.py
+++
@@ -1,5 +0,0 @@
-class Input:
-    def __init__(self, input_type, name, label):
-        self.input_type = input_type
-        self.name = name
-        self.label = label
--- gflow/__main__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-"""gflow.__main__: executed when gflow directory is called as script."""
-
-
-from .gflow import main
-main()
