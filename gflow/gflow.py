7db7e7bde9c5bc337a3894571d31d8b0bf5e0dd1
Alex MacLean <maclean199@gmail.com>
2015-11-26 15:17:48 -0500

    Convert config file to yaml format

--- gflow/gflow.py
+++ gflow/gflow.py
@@ -1,26 +1,35 @@
 import sys
 import json
 import logging
-from input import Input
+import yaml
+import os
 from bioblend.galaxy.objects import *
 
 
 class GFlow(object):
-    """
-    Interact with a Galaxy instance.
 
-    Args:
-        config: An object that holds the parameters pulled from a config file.
-    Attributes:
-        options (dict): A dictionary to hold all the parameters provided by the config argument.
-    """
+    def __init__(self, configfile):
+        """
+        Interact with a Galaxy instance.
 
-    def __init__(self, config):
+        Args:
+            configfile (str): The location of a config file.
+        Attributes:
+            self.logger: For logging.
+            self.cfg (dict): A dictionary to hold all the parameters provided by the config argument.
+            self.galaxy_url (str): The URL of an instance of Galaxy
+            self.galaxy_key (str): The API key of an instance of Galaxy
+        """
         self.logger = logging.getLogger('gflow.GFlow')
-        self.options = config
-        self.__datasets = []
-        for i in range(0, self.options.num_datasets):
-            self.__datasets.append(Input(self.options.dataset_src, self.options.datasets[i], self.options.labels[i]))
+        self.logger.info("Reading config file")
+        with open(configfile, "r") as ymlfile:
+            self.cfg = yaml.load(ymlfile)
+        self.logger.info("Reading Galaxy credentials")
+        self.galaxy_url = os.environ.get("GALAXY_URL", None)
+        self.galaxy_key = os.environ.get("GALAXY_KEY", None)
+        if not self.galaxy_key or not self.galaxy_url:
+            self.logger.error("GALAXY_URL and/or GALAXY_KEY environment variable(s) not set")
+            raise IOError  # TO BE CHANGED
 
     def import_workflow(self, gi):
         """
@@ -31,13 +40,13 @@ class GFlow(object):
         Returns:
             The workflow object created.
         """
-        if self.options.workflow_src == 'local':
-            workflow_json = json.load(open(self.options.workflow))
+        if self.cfg['workflow']['workflow_src'] == 'local':
+            workflow_json = json.load(self.cfg['workflow']['workflow'])
             wf = gi.workflows.import_new(workflow_json)
-        elif self.options.workflow_src == 'id':
-            wf = gi.workflows.get(self.options.workflow)
+        elif self.cfg['workflow']['workflow_src'] == 'id':
+            wf = gi.workflows.get(self.cfg['workflow']['workflow'])
         else:
-            wf = gi.workflows.import_shared(self.options.workflow)          # No shared URL to test yet
+            wf = gi.workflows.import_shared(self.cfg['workflow']['workflow'])          # No shared URL to test yet
         return wf
 
     def import_data(self, library):
@@ -45,21 +54,21 @@ class GFlow(object):
         Import a dataset into a library of an instance of Galaxy.
 
         Args:
-            library (Library): The library to upload the dataset to
+            library (Library): The library to upload the dataset(s) to
         Returns:
             results (LibraryDataset): Dataset object that represents the uploaded content if successful,
                                       None if not successful
         """
         results = None
-        for i in range(0, len(self.__datasets)):
-            self.logger.debug("Dataset source: '%s'" % self.__datasets[i].input_type)
-            if self.__datasets[i].input_type == 'local':
-                results = library.upload_from_local(self.__datasets[i].name)
-            elif self.__datasets[i].input_type == 'url':
-                results = library.upload_from_url(self.__datasets[i].name)      # Need a URL to test
+        for i in range(0, len(self.cfg['input']['datasets'])):
+            self.logger.debug("Dataset source: '%s'" % self.cfg['input']['dataset_src'])
+            if self.cfg['input']['dataset_src'] == 'local':
+                results = library.upload_from_local(self.cfg['input']['datasets']['dataset_' + str(i)]['data'])
+            elif self.cfg['input']['dataset_src'] == 'url':
+                results = library.upload_from_url(self.cfg['input']['datasets'][str(i)]['data'])   # Need a URL to test
             else:
                 try:
-                    results = library.upload_from_galaxy_fs(self.__datasets[i].name)        # Still doesn't work
+                    results = library.upload_from_galaxy_fs(self.cfg['input']['datasets'][str(i)]['data'])
                 except:
                     self.logger.error("File upload unsuccessful, only admins can "
                                       "upload files from the Galaxy filesystem")
@@ -69,14 +78,18 @@ class GFlow(object):
 
     def set_tool_params(self):
         """
-        Map the parameters of tools to the ID of each tool.
+        Map the parameters of tools requiring runtime parameters to the step ID of each tool.
 
         Returns:
-            params (dict): The dictionary containing the tool IDs and parameters.
+            params (dict): The dictionary containing the step IDs and parameters.
         """
         params = {}
-        for i in range(0, self.options.num_tools):
-            params[self.options.step_ids[i]] = self.options.tool_params[i]
+        for i in range(0, len(self.cfg['tool_runtime_params'])):
+            param_dict = {}
+            for j in range(0, len(self.cfg['tool_runtime_params']['tool_' + str(i)]['params'])):
+                param_dict[self.cfg['tool_runtime_params']['tool_' + str(i)]['params']['param_' + str(j)]
+                    ['param_name']] = self.cfg['tool_runtime_params']['tool_0']['params']['param_0']['param_value']
+            params[self.cfg['tool_runtime_params']['tool_' + str(i)]['step_id']] = param_dict
         return params
 
     def run_workflow(self):
@@ -88,7 +101,7 @@ class GFlow(object):
              results (tuple): List of output datasets and output history if successful, None if not successful.
         """
         self.logger.info("Initiating Galaxy connection")
-        gi = GalaxyInstance(self.options.galaxy_url, self.options.galaxy_key)
+        gi = GalaxyInstance(self.galaxy_url, self.galaxy_key)
 
         self.logger.info("Importing workflow")
         workflow = self.import_workflow(gi)
@@ -96,21 +109,21 @@ class GFlow(object):
             self.logger.error("Workflow not runnable, missing required tools")
             return
 
-        self.logger.info("Creating data library '%s'" % self.options.library)
-        library = gi.libraries.create(self.options.library)
+        self.logger.info("Creating data library '%s'" % self.cfg['library'])
+        library = gi.libraries.create(self.cfg['library'])
 
         self.logger.info("Importing data")
         self.import_data(library)
 
-        self.logger.info("Creating output history '%s'" % self.options.outputhist)
-        outputhist = gi.histories.create(self.options.outputhist)
+        self.logger.info("Creating output history '%s'" % self.cfg['output'])
+        outputhist = gi.histories.create(self.cfg['output'])
 
         self.logger.info("Creating input map")
         input_map = dict(zip(workflow.input_labels, library.get_datasets()))
 
         results = None
 
-        if self.options.num_tools:
+        if self.cfg['tool_runtime_params']:
             self.logger.info("Setting runtime tool parameters")
             params = self.set_tool_params()
             self.logger.info("Initiating workflow")
@@ -124,4 +137,5 @@ class GFlow(object):
         else:
             self.logger.error("Workflow did not finish")
 
+        print results
         return results
